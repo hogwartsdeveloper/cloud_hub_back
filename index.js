1b405505d73acf780489a61810267e0f14aacf88
Zhannur <jannuraidynuly@gmail.com>
2022-01-18 21:35:13 +0500

    create cors middleware

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const config = require('config')
 const mongoose = require('mongoose')
 const authRouter = require("./routes/auth.routes")
+const corsMiddleware = require('./middleware/cors.middleware')
 
 const app = express();
 const PORT = process.env.PORT || config.get('serverPort')
 
+app.use(corsMiddleware)
 app.use(express.json())
 app.use("/api/auth", authRouter)
 
