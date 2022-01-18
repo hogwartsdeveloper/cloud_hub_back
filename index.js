cbc5539d8a0028387aafda2112dec2320b41c241
Zhannur <jannuraidynuly@gmail.com>
2022-01-18 19:55:40 +0500

    create model user and connect mongoDb

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const config = require('config')
+const mongoose = require('mongoose')
 
 const app = express();
 const PORT = process.env.PORT || config.get('serverPort')
 
 
-const start = () => {
+const start = async () => {
     try {
+        await mongoose.connect(config.get("dbUrl"))
 
         app.listen(PORT, () => {
             console.log("Server started on port ", PORT)
