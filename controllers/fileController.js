8a74fdee3e6cc0526881766902ef9bb52509b943
Zhannur <jannuraidynuly@gmail.com>
2022-01-19 21:17:43 +0500

    getFiles router

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -23,6 +23,16 @@ class FileController {
             return res.status(400).json(e)
         }
     }
+
+    async getFiles(req, res) {
+        try {
+            let files = await File.find({user: req.user.id, parent: req.query.parent})
+            return res.json(files)
+        } catch(e) {
+            console.log(e)
+            return res.status(500).json({message: "Can not get files"})
+        }
+    }
 }
 
 module.exports = new FileController()
--- routes/file.routes.js
+++ routes/file.routes.js
@@ -4,5 +4,6 @@ const authMiddleware = require('../middleware/auth.middleware')
 const router = new Router()
 
 router.post('', authMiddleware, fileController.createDir)
+router.get('', authMiddleware, fileController.getFiles)
 
 module.exports = router
