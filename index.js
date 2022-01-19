3e4c7d94a5d656b1252b62373485d1685a496a6a
Zhannur <jannuraidynuly@gmail.com>
2022-01-19 18:39:01 +0500

    create create dit controller

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const config = require('config')
 const mongoose = require('mongoose')
 const path = require('path')
 const authRouter = require("./routes/auth.routes")
+const fileRouter = require('./routes/file.routes')
 const corsMiddleware = require('./middleware/cors.middleware')
 const filePathMiddleware = require('./middleware/filePath.middleware')
 
@@ -13,7 +14,7 @@ app.use(corsMiddleware)
 app.use(filePathMiddleware(path.resolve(__dirname, 'files')))
 app.use(express.json())
 app.use("/api/auth", authRouter)
-
+app.use("/api/files", fileRouter)
 
 const start = async () => {
     try {
